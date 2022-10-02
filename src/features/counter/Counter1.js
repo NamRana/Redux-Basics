1a81b8065a0217c381a27ee01c2ba6e0efa2f7b3
Naman <sanjeevnamanrana@gmail.com>
2022-10-02 11:35:22 +0530

    Learning redux basics

---
+++ src/features/counter/Counter1.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Counter(){
+    const [count,setCount]=useState(0);
+    return(
+        <div>
+            <button className="button" aria-label="Increment Value" onClick={()=>{setCount(count+1)}}>+</button>
+            <span className="value">Count:{count}</span>
+            <button className="button" aria-label="Decrement Value" onClick={()=>{setCount(count-1)}}>-</button>
+        </div>
+    )
+}
+
+export default Counter;
